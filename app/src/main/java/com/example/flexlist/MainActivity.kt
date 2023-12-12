5e44e344f75d36ef9f030a8dbb8bfc421ada77bb
alvinjhson <alvinbest26@gmail.com>
2023-12-12 12:45:57 +0100

    new design

--- app/src/main/java/com/example/flexlist/MainActivity.kt
+++ app/src/main/java/com/example/flexlist/MainActivity.kt
@@ -3,6 +3,11 @@ package com.example.flexlist
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
@@ -10,11 +15,33 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton
 class MainActivity : AppCompatActivity() {
     lateinit var recyclerView: RecyclerView
 
+    val dropDownList= arrayOf("Daily List","Shopping List")
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val listSpinner = findViewById<Spinner>(R.id.listSpinner)
+
+        val arrayAdapterDIf = ArrayAdapter<String>(this,R.layout.dropdown,dropDownList)
+        listSpinner.adapter = arrayAdapterDIf
+        listSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                Toast.makeText(applicationContext,"Selected List is " + dropDownList[position],Toast.LENGTH_SHORT).show()
+
+
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+        }
 
 
 
